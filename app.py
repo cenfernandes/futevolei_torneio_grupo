d773db758269fa165516a0716e7afc817cf65e84
Carlos Fernandes <85002655+cenfernandes@users.noreply.github.com>
2026-05-18 09:01:06 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -23,53 +23,53 @@ def index():
     # ===== FASE DE GRUPOS =====
     grupos = {
         "Grupo A": [
-            {"dupla": "Guilherme Yath & Heitor Simões (MEXICO) 1️⃣", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
-            {"dupla": "Rafael & Vinicius Japa Night (CROACIA) 2️⃣", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
-            {"dupla": "João Caovila & Lucca (PORTUGAL)", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla A1  1️⃣", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla A2  2️⃣", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla A3", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
         ],
         
         "Grupo B": [
-            {"dupla": "Arthur Medeiros & Victor (HOLANDA)  1️⃣", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
-            {"dupla": "LC & Luiz Fernando (COLOMBIA) 2️⃣", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
-            {"dupla": "Caio & Hernane (ALEMANHA)", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla B1  1️⃣", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla B2  2️⃣", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla B3", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
         ],
         
         "Grupo C": [
-            {"dupla": "Breno Menezes & Junior (ARGENTINA) 1️⃣", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
-            {"dupla": "Jhonathan & Rafael Gouveia (BÉLGICA) 2️⃣", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
-            {"dupla": "MK & Matheus (INGLATERRA)", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla C1  1️⃣", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla C2  2️⃣", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla C3", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
         ],
         
         "Grupo D": [
-            {"dupla": "Carlinhos & Silvio (ESPANHA) 1️⃣", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
-            {"dupla": "Cadu & João (JAPAO) 2️⃣", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
-            {"dupla": "Diogo & José (URUGUAI)", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla D1  1️⃣", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla D2  2️⃣", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla D3", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
         ]
     }
 
     # ===== ELIMINATÓRIAS =====
     grupo_jogos_A = [
-        "Guilherme Yath & Heitor Simões (MEXICO) 0 x 0 Rafael & Vinicius Japa Night (CROACIA)",
-        "Guilherme Yath & Heitor Simões (MEXICO) 0 x 0 João Caovila & Lucca (PORTUGAL)",
-        "Rafael & Vinicius Japa Night (CROACIA) 0 x 0 João Caovila & Lucca (PORTUGAL)"
+        "Dupla A1 0 x 0 Dupla A2",
+        "Dupla A1 0 x 0 Dupla A3",
+        "Dupla A2 0 x 0 Dupla A3"
     ]
     
     grupo_jogos_B = [
-        "Arthur Medeiros & Victor (HOLANDA) 0 x 0 LC & Luiz Fernando (COLOMBIA)",
-        "Arthur Medeiros & Victor (HOLANDA) 0 x 0 Caio & Hernane (ALEMANHA)",
-        "LC & Luiz Fernando (COLOMBIA) 0 x 0 Caio & Hernane (ALEMANHA)"
+        "Dupla B1 0 x 0 Dupla B2",
+        "Dupla B1 0 x 0 Dupla B3",
+        "Dupla B2 0 x 0 Dupla B3"
     ]
     
     grupo_jogos_C = [
-        "Breno Menezes & Junior (ARGENTINA) 0 x 0 Jhonathan & Rafael Gouveia (BÉLGICA)",
-        "Breno Menezes & Junior (ARGENTINA) 0 x 0 MK & Matheus (INGLATERRA)",
-        "Jhonathan & Rafael Gouveia (BÉLGICA) 0 x 0 MK & Matheus (INGLATERRA)"
+        "Dupla C1 0 x 0 Dupla C2",
+        "Dupla C1 0 x 0 Dupla C3",
+        "Dupla C2 0 x 0 Dupla C3"
     ]
     
     grupo_jogos_D = [
-        "Carlinhos & Silvio (ESPANHA) 0 x 0 Cadu & João (JAPAO)",
-        "Carlinhos & Silvio (ESPANHA) 0 x 0 Diogo & José (URUGUAI)",
-        "Cadu & João (JAPAO) 0 x 0 Diogo & José (URUGUAI)"
+        "Dupla D1 0 x 0 Dupla D2",
+        "Dupla D1 0 x 0 Dupla D3",
+        "Dupla D2 0 x 0 Dupla D3"
     ]
     
     quartas = [
