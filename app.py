eb0b5d9b977399be67a7f38dab2ae4bcb7729dfb
Carlos Fernandes <85002655+cenfernandes@users.noreply.github.com>
2026-07-12 10:04:59 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -20,55 +20,89 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 
-duplas = [
+    # ===== FASE DE GRUPOS =====
+    grupos = {
+        "Grupo A": [
+            {"dupla": "Dupla A1", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla A2", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla A3", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+        ],
+        
+        "Grupo B": [
+            {"dupla": "Dupla B1", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla B2", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla B3", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+        ],
+        
+        "Grupo C": [
+            {"dupla": "Dupla C1", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla C2", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla C3", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+        ],
+        
+        "Grupo D": [
+            {"dupla": "Dupla D1", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla D2", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+            {"dupla": "Dupla D3", "vitorias": 0, "derrotas": 0, "pro": 0, "contra": 0, "saldo": 0},
+        ]
+    }
 
-    "Dupla 1",
-    "Dupla 2",
+    # ===== ELIMINATÓRIAS =====
+    grupo_jogos_A = [
+        "Dupla A1 0 x 0 Dupla A2",
+        "Dupla A1 0 x 0 Dupla A3",
+        "Dupla A2 0 x 0 Dupla A3"
+    ]
+    
+    grupo_jogos_B = [
+        "Dupla B1 0 x 0 Dupla B2",
+        "Dupla B1 0 x 0 Dupla B3",
+        "Dupla B2 0 x 0 Dupla B3"
+    ]
+    
+    grupo_jogos_C = [
+        "Dupla C1 0 x 0 Dupla C2",
+        "Dupla C1 0 x 0 Dupla C3",
+        "Dupla C2 0 x 0 Dupla C3"
+    ]
+    
+    grupo_jogos_D = [
+        "Dupla D1 0 x 0 Dupla D2",
+        "Dupla D1 0 x 0 Dupla D3",
+        "Dupla D2 0 x 0 Dupla D3"
+    ]
+    
+    quartas = [
+        "Dupla A1 0 x 0 Dupla B2",
+        "Dupla B1 0 x 0 Dupla A2",
+        "Dupla C1 0 x 0 Dupla D2",
+        "Dupla D1 0 x 0 Dupla C2"
+    ]
 
-    "Dupla 3",
-    "Dupla 4",
+    semifinais = [
+        "Vencedor Quarta 1 0 x 0 Vencedor Quarta 3",
+        "Vencedor Quarta 2 0 x 0 Vencedor Quarta 4"
+    ]
 
-    "Dupla 5",
-    "Dupla 6",
+    terceiro = [
+        "Perdedor Semifinal 1 0 x 0 Perdedor Semifinal 2"
+    ]
 
-    "Dupla 7",
-    "Dupla 8",
+    final = [
+        "Vencedor Semifinal 1 0 x 0 Vencedor Semifinal 2"
+    ]
 
-    "Dupla 9",
-    "Dupla 10",
-
-    "Dupla 11",
-    "Dupla 12",
-
-    "Dupla 13",
-    "Dupla 14",
-
-    "Dupla 15",
-    "Dupla 16"
-]
-
-jogos_iniciais = [
-
-    (duplas[0], duplas[1]),
-
-    (duplas[2], duplas[3]),
-
-    (duplas[4], duplas[5]),
-
-    (duplas[6], duplas[7]),
-
-    (duplas[8], duplas[9]),
-
-    (duplas[10], duplas[11]),
-
-    (duplas[12], duplas[13]),
-
-    (duplas[14], duplas[15])
-
-]
     return render_template(
         "index.html",
-        jogos=jogos_iniciais
+        grupos=grupos,
+        grupo_jogos_A = grupo_jogos_A,
+        grupo_jogos_B = grupo_jogos_B,
+        grupo_jogos_C = grupo_jogos_C,
+        grupo_jogos_D = grupo_jogos_D,
+        quartas=quartas,
+        semifinais=semifinais,
+        terceiro=terceiro,
+        final=final
     )
 
 
